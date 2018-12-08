42146289f3e237891aca07b54c52232fab8e8d3e
khanhtran86 <khanh.tx@live.com>
2018-12-08 10:26:07 +0700

    firs script complte

--- seleniumdemo/src/test/java/testmaster/seleniumdemo/AppTest.java
+++ seleniumdemo/src/test/java/testmaster/seleniumdemo/AppTest.java
@@ -3,18 +3,30 @@ package testmaster.seleniumdemo;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+import pageobject.loginpage;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    WebDriver driver;
+    
     @Test
-    public void shouldAnswerWithTrue()
+    public void firstTestCase()
     {
+    	System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
+    	driver= new ChromeDriver();
+    	
+    	driver.get("https://member.lazada.vn/user/login?spm=a2o4n.home.header.d5.19056afeWNz03N&redirect=https%3A%2F%2Fwww.lazada.vn%2F");
+    	
+    	//Nhap Username
+    	loginpage lgPage = new loginpage(driver);
+    	lgPage.txtUserName.sendKeys("khanh.tx@live.com");
+    	
         assertTrue( true );
     }
 }
