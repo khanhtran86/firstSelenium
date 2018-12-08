229aaa115fcd6aa86f2081beea1e65628f6edd73
khanhtran86 <khanh.tx@live.com>
2018-12-08 10:02:40 +0700

    create project

---
+++ seleniumdemo/src/test/java/testmaster/seleniumdemo/AppTest.java
@@ -0,0 +1,20 @@
+package testmaster.seleniumdemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
