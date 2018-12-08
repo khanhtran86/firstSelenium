699e9ba05a924dee25346d0de5af65d352c6bf4a
khanhtran86 <khanh.tx@live.com>
2018-12-08 16:41:56 +0700

    lazada login

--- seleniumdemo/src/test/java/pageobject/loginpage.java
+++ seleniumdemo/src/test/java/pageobject/loginpage.java
@@ -9,11 +9,18 @@ public class loginpage {
 	
 	@FindBy(css="div.mod-login-input-loginName>input")
 	public WebElement txtUserName;
+	@FindBy(css="div.mod-login-input-loginName>span")
+	public WebElement lbUserNameError;
 	@FindBy(css="div.mod-login-input-password>input[type=password]")
 	public WebElement txtPassword;
+	@FindBy(css="div.mod-login-input-password>span")
+	public WebElement lbPasswordError;
 	@FindBy(css="button.next-btn-primary")
-	public WebElement btnSubmit;
-	
+	public WebElement btnLogin;
+	@FindBy(css="div.next-feedback-toast")
+	public WebElement pnlNotifier;
+	@FindBy(css="div.next-feedback-toast .next-feedback-content")
+	public WebElement lbNotification;
 	
 	public loginpage(WebDriver driver)
 	{
--- seleniumdemo/src/test/java/testmaster/seleniumdemo/AppTest.java
+++ seleniumdemo/src/test/java/testmaster/seleniumdemo/AppTest.java
@@ -1,10 +1,15 @@
 package testmaster.seleniumdemo;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import pageobject.loginpage;
 
@@ -13,20 +18,69 @@ import pageobject.loginpage;
  */
 public class AppTest 
 {
-    WebDriver driver;
+	WebDriver driver;
+    loginpage loginPage;
+	@Before
+	public void InitTest()
+	{
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://member.lazada.vn/user/login");
+		this.loginPage = new loginpage(this.driver);
+	}
+	
+	@After
+	public void CloseTest()
+	{
+		this.driver.close();
+	}
+	
+	@Ignore
+    @Test
+    public void Check_Login_With_Blank_Values()
+    {
+    	loginPage.txtUserName.clear();
+    	loginPage.txtPassword.clear();
+    	loginPage.btnLogin.click();
+    	
+    	String userNameError = loginPage.lbUserNameError.getText();
+    	String passwordError = loginPage.lbPasswordError.getText();
+    	
+    	Boolean result = userNameError.indexOf("can't leave this empty")!=-1;
+    	assertTrue(result);
+    	/*
+    	assertEquals(userNameError, "You can't leave this empty.");
+    	assertEquals(passwordError, "You can't leave this empty.");
+    	*/
+    }
+    
+    @Ignore
+    @Test
+    public void Check_login_with_valid_values()
+    {
+    	loginPage.txtUserName.clear();
+    	loginPage.txtPassword.clear();
+    	loginPage.btnLogin.click();
+    	
+    	loginPage.txtUserName.sendKeys("khanh.tx@live.com");
+    	String userNameError = loginPage.lbUserNameError.getText();
+    	assertEquals(userNameError, "");
+    	
+    	loginPage.txtUserName.sendKeys("123456abc");
+    	String pwdError = loginPage.lbPasswordError.getText();
+    	assertEquals(pwdError, "");
+    	
+    }
     
     @Test
-    public void firstTestCase()
-    {
-    	System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
-    	driver= new ChromeDriver();
+    public void Check_login_with_account_not_existed(){
+    	loginPage.txtUserName.sendKeys("khanh.tx999");
+    	loginPage.txtPassword.sendKeys("abc123");
+    	loginPage.btnLogin.click();
     	
-    	driver.get("https://member.lazada.vn/user/login?spm=a2o4n.home.header.d5.19056afeWNz03N&redirect=https%3A%2F%2Fwww.lazada.vn%2F");
-    	
-    	//Nhap Username
-    	loginpage lgPage = new loginpage(driver);
-    	lgPage.txtUserName.sendKeys("khanh.tx@live.com");
-    	
-        assertTrue( true );
+    	assertTrue(loginPage.pnlNotifier.isDisplayed());
+    	assertEquals(loginPage.lbNotification.getText(), "Please enter a valid phone number.");
     }
+    
+    
 }
